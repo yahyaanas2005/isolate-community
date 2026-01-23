ea5ccfa80772141bd97d89b4d7bc18a9924549ec
Yahya Anas <yahyaanas2005@gmail.com>
2026-01-23 15:27:58 +0500

    chore(debug): temporarily disable middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
+// export async function middleware(request: NextRequest) {
+//     return await updateSession(request);
+// }
+
 export async function middleware(request: NextRequest) {
-    return await updateSession(request);
+    return NextResponse.next();
 }
 
 export const config = {
