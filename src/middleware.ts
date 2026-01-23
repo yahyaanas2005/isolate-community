b1405737bfec7ce2dc12e55102f27620b42e8653
Yahya Anas <yahyaanas2005@gmail.com>
2026-01-23 15:31:01 +0500

    chore: strictly remove middleware dependency on utils

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,4 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
-
-// export async function middleware(request: NextRequest) {
-//     return await updateSession(request);
-// }
 
 export async function middleware(request: NextRequest) {
     return NextResponse.next();
@@ -11,13 +6,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
-         */
         "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     ],
 };
